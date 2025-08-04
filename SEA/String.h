202b67a127c70f94b93a1d983b6eefa6fd332f34
DWA <dylan.warlus@spirtech.com>
2025-08-04 17:21:11 +0200

    Refactor: improve `SEA_strncpy_s` compatibility by adding MinGW support and reorganizing implementation

--- SEA/String.h
+++ SEA/String.h
@@ -3,25 +3,10 @@
 
 #include <string.h>
 
-#if defined(__STDC_LIB_EXT1__) || (defined(_MSC_VER) && _MSC_VER >= 1400)
-  #define SEA_strncpy_s(dest_, size_, src_, count_) strncpy_s((dest_), (size_), (src_), (count_))
+#if defined(__STDC_LIB_EXT1__) || (defined(_MSC_VER) && _MSC_VER >= 1400) || defined(__MINGW32__)
+#define SEA_strncpy_s(dest_, size_, src_, count_) strncpy_s((dest_), (size_), (src_), (count_))
 #else
-#include <errno.h>
-static inline int SEA_strncpy_s(char *restrict dest, const size_t destsz, const char *restrict src, const size_t count) {
-	if (!dest || !src || destsz == 0) return EINVAL;
-	if (count > destsz) {
-		if (destsz > 0) dest[0] = '\0';
-		return ERANGE;
-	}
-
-	size_t i = 0;
-	for (; i < count && src[i] != '\0'; ++i)
-		dest[i] = src[i];
-	if (i < destsz) dest[i] = '\0';
-	else dest[destsz - 1] = '\0';
-
-	return 0;
-}
+int SEA_strncpy_s(char *restrict dest, const size_t destsz, const char *restrict src, const size_t count);
 #endif
 
 #endif // SEA_STRING_H
