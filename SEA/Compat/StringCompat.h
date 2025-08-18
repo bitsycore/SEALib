041d0d8dd40c6540e5b2f8598cf1dc7dcbb2ea09
bitsy <bitsydoge@proton.me>
2025-08-18 08:11:58 +0200

    Refactor: reorganize `SEA_strcpy_s` and `SEA_strncpy_s` definitions for consistent conditional handling and improved readability

--- SEA/Compat/StringCompat.h
+++ SEA/Compat/StringCompat.h
@@ -4,15 +4,15 @@
 #include <string.h>
 
 #if defined(__STDC_LIB_EXT1__) || (defined(_MSC_VER) && _MSC_VER >= 1400) || defined(__MINGW32__)
-#define SEA_strncpy_s(dest_, size_, src_, count_) strncpy_s((dest_), (size_), (src_), (count_))
-#else
-int SEA_strncpy_s(char *restrict dest, size_t destsz, const char *restrict src, size_t count);
-#endif
 
-#if defined(__STDC_LIB_EXT1__) || (defined(_MSC_VER) && _MSC_VER >= 1400) || defined(__MINGW32__)
 #define SEA_strcpy_s(dest_, destsize_, src_) strncpy_s((dest_), (destsize_), (src_))
+#define SEA_strncpy_s(dest_, size_, src_, count_) strncpy_s((dest_), (size_), (src_), (count_))
+
 #else
+
 int SEA_strcpy_s(char* restrict dest, size_t destsz, const char* restrict src);
+int SEA_strncpy_s(char *restrict dest, size_t destsz, const char *restrict src, size_t count);
+
 #endif
 
 #endif//SEA_STRINGCOMPAT_H
